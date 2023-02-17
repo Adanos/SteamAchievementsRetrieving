6b3fae5adb08bce17b759e5879d7029e7e9aa055
Adanos <polon1@op.pl>
2023-02-17 15:27:54 +0100

    Adding unit tests

---
+++ SteamAchievementsRetrieving/MatchingManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SteamAchievementsRetrieving
+{
+    public class MatchingManager
+    {
+        public string FindMatching(string text)
+        {
+            string pattern = @"\bas\b\s[a-zA-Z]*(\s\bor\b[a-zA-Z\s]*)?";
+            string result = null;
+
+            Match match = Regex.Match(text, pattern, RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                result = match.Captures[0].Value;
+                result = result?.Replace("as", "")?.Replace("As", "")?.Replace(" or", ",").Trim();
+            }
+
+            return result;
+        }
+    }
+}
