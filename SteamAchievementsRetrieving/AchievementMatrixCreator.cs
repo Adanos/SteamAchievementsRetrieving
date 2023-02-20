6167c3101541aa248a7208267c0646885bb37ea6
Adanos <polon1@op.pl>
2023-02-20 14:44:10 +0100

    fixing regex

---
+++ SteamAchievementsRetrieving/AchievementMatrixCreator.cs
@@ -0,0 +1,34 @@
+﻿using SteamAchievementsRetrieving.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SteamAchievementsRetrieving
+{
+    public class AchievementMatrixCreator
+    {
+        private IList<Achievement> Achievements { get; }
+        private IDictionary<string, List<string>> matrix = new Dictionary<string, List<string>>();
+        public AchievementMatrixCreator(IList<Achievement> achievements)
+        {
+            Achievements = achievements;
+        }
+
+        public void CreateMatrix()
+        {
+            MatchingManager matchingManager = new MatchingManager();
+
+            foreach (var achievement in Achievements)
+            {
+                var countries = matchingManager.FindCountryMatching(achievement.Description)?.Split(",").Where(x => !string.IsNullOrEmpty(x));
+
+                foreach (var country in countries ?? Enumerable.Empty<string>())
+                {
+                    bool exists = matrix.TryGetValue(country, out List<string> value);
+                    if (exists)
+                        value.Add(achievement.Name);
+                    else matrix.Add(country, new List<string>() { achievement.Name });
+                }
+            }
+        }
+    }
+}
