3eeb6d21c0354c1fe8e26506bc6041891e6c4e0b
Adanos <GrzegorzGasior@protonmail.com>
2023-02-21 15:50:01 +0100

    merge the same keys, ignoring case sensitive, adding unit tests

--- SteamAchievementsRetrieving/AchievementMatrixCreator.cs
+++ SteamAchievementsRetrieving/AchievementMatrixCreator.cs
@@ -1,16 +1,18 @@
 ﻿using SteamAchievementsRetrieving.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SteamAchievementsRetrieving
 {
     public class AchievementMatrixCreator
     {
         private IList<Achievement> Achievements { get; }
-        private IDictionary<string, List<string>> Matrix = new Dictionary<string, List<string>>();
+        public IDictionary<string, List<string>> Matrix { get; private set; }
         public AchievementMatrixCreator(IList<Achievement> achievements)
         {
             Achievements = achievements;
+            Matrix = new Dictionary<string, List<string>>();
         }
 
         public void CreateMatrix()
@@ -28,6 +30,29 @@ namespace SteamAchievementsRetrieving
                         value.Add(achievement.Name);
                     else Matrix.Add(country, new List<string>() { achievement.Name });
                 }
+            }
+
+            Matrix = Matrix.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
+
+            MergeTheSameKeys();
+        }
+
+        private void MergeTheSameKeys()
+        {
+            IDictionary<string, List<string>> newMatrix = new Dictionary<string, List<string>>();
+
+            var duplicateKeys = Matrix.Keys.Where(x => Regex.Match(x, @"(\bthe\b)|(\bThe\b).*").Success).ToList();
+
+            foreach (var item in duplicateKeys)
+            {
+                string keyToChange = item.Replace("The ", "").Replace("the ", "");
+                Matrix.TryGetValue(item, out List<string> values);
+                if (Matrix.ContainsKey(keyToChange))
+                {
+                    Matrix[keyToChange].AddRange(values);               
+                }
+                else Matrix.Add(keyToChange, values);
+                Matrix.Remove(item);
             }
         }
     }
---
+++ SteamAchievementsRetrievingTests/AchievementMatrixCreatorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SteamAchievementsRetrieving;
+using SteamAchievementsRetrieving.Models;
+using System.Collections.Generic;
+
+namespace SteamAchievementsRetrievingTests
+{
+    class AchievementMatrixCreatorTests
+    {
+        private IList<Achievement> Achievements { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            Achievements = new List<Achievement>()
+            {
+                new Achievement() { Name = "Test 1", Description = "As the Knights conquer the world." },
+                new Achievement() { Name = "Test 2", Description = "As The Knights have 1000 gold." },
+                new Achievement() { Name = "Test 3", Description = "Win a war as Cyprus." }
+            };
+        }
+
+        [Test]
+        public void MergeTheSameKeys_KeysAreSimilarCaseSensitive_ReturnOneKey()
+        {
+            AchievementMatrixCreator achievementMatrixCreator = new AchievementMatrixCreator(Achievements);
+            achievementMatrixCreator.CreateMatrix();
+            achievementMatrixCreator.Matrix.TryGetValue("Knights", out List<string> values);
+
+            Assert.IsTrue(achievementMatrixCreator.Matrix.ContainsKey("Knights"));
+            Assert.IsFalse(achievementMatrixCreator.Matrix.ContainsKey("the Knights"));
+            Assert.IsFalse(achievementMatrixCreator.Matrix.ContainsKey("The Knights"));
+            Assert.AreEqual(2, values.Count);
+        }
+    }
+}
