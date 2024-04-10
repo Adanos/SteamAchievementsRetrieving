6976a4359f71b87a72ae0fdb4011cb033eed9d73
Adanos <GrzegorzGasior@protonmail.com>
2024-04-10 16:55:23 +0200

    issue #4

--- SteamAchievementsRetrieving/IO/SaveFileManager.cs
+++ SteamAchievementsRetrieving/IO/SaveFileManager.cs
@@ -24,10 +24,26 @@ namespace SteamAchievementsRetrieving.IO
         {
             StringBuilder stringBuilder = new StringBuilder(Constants.HeaderOfCsvFile + string.Join(";", DlcNames) + Environment.NewLine);
             foreach (var achievement in Achievements ?? Enumerable.Empty<Achievement>())
+            {
                 stringBuilder.Append($"{achievement.Name}{Constants.Separator}{achievement.Description}{Constants.Separator}{achievement.Country}{Constants.Separator}" +
-                    $"{achievement.IsRequiredDlc}{Constants.Separator}{string.Join(Constants.Separator, achievement?.AllRequiredDlcNames ?? [])}{Environment.NewLine}");
+                    $"{achievement.IsRequiredDlc}{Constants.Separator}{MarkDlcWithOperator(achievement)}{Environment.NewLine}");
+            }
 
             return stringBuilder.ToString();
+        }
+
+        private string MarkDlcWithOperator(Achievement achievement)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var dlcName in DlcNames)
+            {
+                if (achievement?.AllRequiredDlcNames != null && achievement.AllRequiredDlcNames.Contains(dlcName))
+                    result.Append($"AND{Constants.Separator}");
+                else if (achievement?.OneRequiredOfDlcNames != null && achievement.OneRequiredOfDlcNames.Contains(dlcName))
+                    result.Append($"OR{Constants.Separator}");
+                else result.Append(Constants.Separator);
+            }
+            return result.ToString();
         }
 
         public void SaveCsvFile()
