403829b2b252f771239adc574a6f74f471a17c39
Adanos <GrzegorzGasior@protonmail.com>
2024-04-10 15:21:04 +0200

    issue #3

--- SteamAchievementsRetrieving/IO/ReadFileManager.cs
+++ SteamAchievementsRetrieving/IO/ReadFileManager.cs
@@ -19,7 +19,7 @@ namespace SteamAchievementsRetrieving.IO
                 string[] words = line.Split(Constants.Separator);
                 bool.TryParse(words[3], out bool isRequiredDlc);
                 IList<string> dlcNames = words[4..].Where(x => !string.IsNullOrEmpty(x)).ToList();
-                achievements.Add(new Achievement() { Name = words[0], Description = words[1], Country = words[2], IsRequiredDlc = isRequiredDlc, DlcNames = dlcNames });
+                achievements.Add(new Achievement() { Name = words[0], Description = words[1], Country = words[2], IsRequiredDlc = isRequiredDlc, AllRequiredDlcNames = dlcNames });
             }
 
             return achievements;
