fdbb9826c54339a94545dcfd858b804a1943d5bb
Adanos <GrzegorzGasior@protonmail.com>
2025-01-23 16:01:28 +0100

    adding configuration for Gog and adding unit tests, fixing classes

--- SteamAchievementsRetrieving/Models/FromApi/SteamAchievementResponse.cs
+++ SteamAchievementsRetrieving/Models/FromApi/SteamAchievementResponse.cs
@@ -4,8 +4,9 @@ namespace SteamAchievementsRetrieving.Models.FromApi
 {
     public class SteamAchievementResponse
     {
-        public HttpStatusCode StatusCode { get; internal set; }
+        public HttpStatusCode StatusCode { get; set; }
         public PlayerStats PlayerStats { get; set; }
         public bool Success { get; set; }
+        public string ErrorMessage { get; internal set; }
     }
 }
